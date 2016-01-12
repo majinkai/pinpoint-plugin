2d2c50a2c7a515dcc74484af981d6c96d1fb0ccf
Jinkai.Ma <majinkai@handu.com>
2016-01-12 18:24:31 +0800

    Add plugin for RabbitMQ provider side

--- plugins/rabbitmq/src/main/java/com/handu/apollo/pinpoint/plugin/rabbitmq/RabbitMQPlugin.java
+++ plugins/rabbitmq/src/main/java/com/handu/apollo/pinpoint/plugin/rabbitmq/RabbitMQPlugin.java
@@ -1,9 +1,16 @@
 package com.handu.apollo.pinpoint.plugin.rabbitmq;
 
+import com.navercorp.pinpoint.bootstrap.instrument.InstrumentClass;
+import com.navercorp.pinpoint.bootstrap.instrument.InstrumentException;
+import com.navercorp.pinpoint.bootstrap.instrument.InstrumentMethod;
+import com.navercorp.pinpoint.bootstrap.instrument.Instrumentor;
+import com.navercorp.pinpoint.bootstrap.instrument.transformer.TransformCallback;
 import com.navercorp.pinpoint.bootstrap.instrument.transformer.TransformTemplate;
 import com.navercorp.pinpoint.bootstrap.instrument.transformer.TransformTemplateAware;
 import com.navercorp.pinpoint.bootstrap.plugin.ProfilerPlugin;
 import com.navercorp.pinpoint.bootstrap.plugin.ProfilerPluginSetupContext;
+
+import java.security.ProtectionDomain;
 
 /**
  * @author Jinkai.Ma
@@ -17,7 +24,32 @@ public class RabbitMQPlugin implements ProfilerPlugin, TransformTemplateAware {
     }
 
     private void addTransformers() {
+        transformTemplate.transform("com.rabbitmq.client.impl.recovery.AutorecoveringChannel", new TransformCallback() {
+            @Override
+            public byte[] doInTransform(Instrumentor instrumentor, ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws InstrumentException {
+                InstrumentClass target = instrumentor.getInstrumentClass(loader, className, classfileBuffer);
 
+                final InstrumentMethod method = target.getDeclaredMethod("basicPublish", "java.lang.String", "java.lang.String", "boolean", "boolean", "com.rabbitmq.client.AMQP$BasicProperties", "byte[]");
+                if (method != null) {
+                    method.addInterceptor("com.handu.apollo.pinpoint.plugin.rabbitmq.interceptor.RabbitMQPublishInterceptor");
+                }
+
+                return target.toBytecode();
+            }
+        });
+        transformTemplate.transform("com.rabbitmq.client.impl.ChannelN", new TransformCallback() {
+            @Override
+            public byte[] doInTransform(Instrumentor instrumentor, ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws InstrumentException {
+                InstrumentClass target = instrumentor.getInstrumentClass(loader, className, classfileBuffer);
+
+                final InstrumentMethod method = target.getDeclaredMethod("basicPublish", "java.lang.String", "java.lang.String", "boolean", "boolean", "com.rabbitmq.client.AMQP$BasicProperties", "byte[]");
+                if (method != null) {
+                    method.addInterceptor("com.handu.apollo.pinpoint.plugin.rabbitmq.interceptor.RabbitMQPublishInterceptor");
+                }
+
+                return target.toBytecode();
+            }
+        });
     }
 
     @Override
--- plugins/rabbitmq/src/main/java/com/handu/apollo/pinpoint/plugin/rabbitmq/RabbitMQTraceMetadataProvider.java
+++ plugins/rabbitmq/src/main/java/com/handu/apollo/pinpoint/plugin/rabbitmq/RabbitMQTraceMetadataProvider.java
@@ -9,6 +9,11 @@ import com.navercorp.pinpoint.common.trace.TraceMetadataSetupContext;
 public class RabbitMQTraceMetadataProvider implements TraceMetadataProvider {
     @Override
     public void setup(TraceMetadataSetupContext context) {
+        context.addServiceType(RabbitMQConstants.RABBITMQ_SERVICE_TYPE);
 
+        context.addAnnotationKey(RabbitMQConstants.RABBITMQ_EXCHANGE_ANNOTATION_KEY);
+        context.addAnnotationKey(RabbitMQConstants.RABBITMQ_ROUTINGKEY_ANNOTATION_KEY);
+        context.addAnnotationKey(RabbitMQConstants.RABBITMQ_PROPERTIES_ANNOTATION_KEY);
+        context.addAnnotationKey(RabbitMQConstants.RABBITMQ_BODY_ANNOTATION_KEY);
     }
 }
